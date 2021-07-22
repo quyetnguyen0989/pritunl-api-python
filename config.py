c2749da1b364880f52d646d2084d3ae9f35c4ee4
Nguyen Van Quyet <23342055+quyetnguyen0989@users.noreply.github.com>
2021-07-22 22:20:51 +0700

    update delete users and get id

--- config.py
+++ config.py
@@ -1,4 +1,25 @@
 from pritunl_api import *
+import pprint
+pprint.pprint(json)
 pri = Pritunl(url="https://yoursite.com",
             token="###",
             secret="###")
+
+# delete user
+org_id = ""
+users = pri.user.get(org_id=org_id)
+email_delete = "quyet@abc.com"
+for user in users:
+    if user["name"] == email_delete:
+        pri.user.delete(org_id=user["organization"], usr_id=user["id"])
+
+
+print(pri.ping())
+# view org 
+q = pri.user.get(org_id="")
+# View users id
+q = pri.user.get(org_id=x[0]['id'])
+# Delete users
+pri.user.delete(org_id=x[0]['id'], user_id=q[1]['id'])
+
+print(q)
